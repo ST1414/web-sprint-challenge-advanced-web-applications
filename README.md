f9d02a80250bb0ff7e64265b738cc3d2c43c417c
Sean Terry <seanterry@Seans-MBP.localdomain>
2021-11-12 13:29:02 -0800

    Test 1, 2, and 3 are complete

--- README.md
+++ README.md
@@ -64,12 +64,12 @@ In this project, you will demonstrate your mastery of these skills by creating *
 ### Logout Authentication
 > *Add in the http requests needed to logout of the application.*
 
-* [ ] In `Logout.js`, execute a http request to logout on mount. When the request is complete, the user's security token should be removed and the browser should redirect to the login page.
+* [x] In `Logout.js`, execute a http request to logout on mount. When the request is complete, the user's security token should be removed and the browser should redirect to the login page.
 
 ### Advanced Testing
 > *Add the following tests within Article.test.js.*
-* [ ] Build a test that shows the `Article` component, given the correct props, can render without errors.
-* [ ] Build a test that shows that when a correctly formatted article is passed into the `Article` component, the correct headline, author, summary and body are displayed.
+* [x] Build a test that shows the `Article` component, given the correct props, can render without errors.
+* [x] Build a test that shows that when a correctly formatted article is passed into the `Article` component, the correct headline, author, summary and body are displayed.
 * [ ] The `Article` component should display "Associated Press" when an author attribute is not avalible. Build a test that verifies that that is true.
 * [ ] Build a test that show that when the deleteButton is pressed on an Article, the handleDelete functional property is executed.
 
--- src/components/Article.test.js
+++ src/components/Article.test.js
@@ -1,22 +1,62 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-
+import { render, screen, waitFor } from '@testing-library/react'; // Added
 import userEvent from '@testing-library/user-event';
-import MutationObserver from 'mutationobserver-shim';
 
 import Article from './Article';
 
-test('renders component without errors', ()=> {
+
+const article = {
+    id: 123,
+    headline: "Less than half of Seattle homes have air conditioning. After a deadly heat wave, ‘everybody’ wants it.",
+    createdOn: Date.now(),
+    author:"",
+    image: 134,
+    summary: "Triple-digit temperatures led t.",
+    body: "Inside the attic of a one-story gray house ."   
+};
+const handleDelete = () => {
+    
+}
+const handleEditSelect = () => {
+
+}
+
+test('renders component without errors', () => {
+
+    render(<Article key={article.id} article={article} handleDelete={handleDelete} handleEditSelect={handleEditSelect} />)
+
 });
 
-// test('renders headline, author from the article when passed in through props', ()=> {
-// });
+test('renders headline, author from the article when passed in through props', ()=> {
 
-// test('renders "Associated Press" when no author is given', ()=> {
-// });
+    render(<Article key={article.id} article={article} handleDelete={handleDelete} handleEditSelect={handleEditSelect} />)
 
-// test('executes handleDelete when the delete button is pressed', ()=> {
-// });
+    const headline = screen.getByTestId('headline');
+    const author = screen.getByTestId('author');
+    const summary = screen.getByTestId('summary');
+    const body = screen.getByTestId('body');
+
+    expect(headline).toBeInTheDocument();
+    expect(author).toBeInTheDocument();
+    expect(summary).toBeInTheDocument();
+    expect(body).toBeInTheDocument();
+
+});
+
+test('renders "Associated Press" when no author is given', ()=> {
+
+    render(<Article key={article.id} article={article} handleDelete={handleDelete} handleEditSelect={handleEditSelect} />)
+
+    const author = screen.getByText(/Associated Press/i);
+
+    expect(author).toBeTruthy;
+
+});
+
+test('executes handleDelete when the delete button is pressed', ()=> {
+    
+});
 
 //Task List:
 //1. Complete all above tests. Create test article data when needed.
