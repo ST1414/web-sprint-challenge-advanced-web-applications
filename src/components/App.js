c8f6375821a0d44b8d12051e4de18ea52c11d98e
Sean Terry <seanterry@Seans-MBP.localdomain>
2021-11-12 09:36:07 -0800

    Built routes in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,16 @@ const App = () => {
       <RouteContainer>
         <Route exact path="/">
           <Login/>
-        </Route>          
+        </Route>
+        <Route path="/login">
+          <Login/>
+        </Route>    
+        <Route path="/view">
+          <View/>
+        </Route>      
+        <Route path="/logout">
+          <Logout/>
+        </Route>      
       </RouteContainer>
     </AppContainer>
   )
